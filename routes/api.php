a3207897963ce884423cef3466fb5020b8780ee0
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-11-05 23:34:25 +0600

    resource user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/users', function () {
+    return UserResource::collection(User::all());
+});
+
+Route::get('/user/{id}', function (string $id) {
+    return new UserResource(User::findOrFail($id));
 });
