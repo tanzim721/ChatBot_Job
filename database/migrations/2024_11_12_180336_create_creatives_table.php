da375745364ef6d56f49dcdb0b43ee990107b6ae
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-11-13 00:09:48 +0600

    Creative model and controlle create

---
+++ database/migrations/2024_11_12_180336_create_creatives_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('creatives', function (Blueprint $table) {
+            $table->id();
+            $table->json('image');
+            $table->json('video');
+            $table->string('content')->nullable();
+            $table->string('cta_name')->nullable();
+            $table->string('cta_url')->nullable();
+            $table->string('creative_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('creatives');
+    }
+};
